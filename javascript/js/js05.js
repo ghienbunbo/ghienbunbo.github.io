3c58e63dba7c8b2183b6454ab05eb13b287a45a5
Dangkhoi <khoinam321@gmail.com>
2024-08-26 22:44:12 +0700

    add midtest and jQ

--- javascript/js/js05.js
+++ javascript/js/js05.js
@@ -2,7 +2,7 @@ function bcc(){
     for(i=2;i<10;i++){
         var cell="";
         for(j=2;j<10;j++){
-            cell+=(`${Number(i)}  x  ${Number(j)} =  ${Number(i*j)} <br>`);
+            cell+=`${Number(i)}  x  ${Number(j)} =  ${Number(i*j)} <br>`;
         }
         document.getElementById('t'+i).innerHTML = cell;
     }
